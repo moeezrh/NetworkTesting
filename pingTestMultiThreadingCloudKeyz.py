df520619afa00b15cf5b71003a58761e0419aac3
Moeez <moeez123104@gmail.com>
2023-08-08 16:26:55 -0700

    Improved code readability with comments as well as starting to break down code into functions

--- pingTestMultiThreadingCloudKeyz.py
+++ pingTestMultiThreadingCloudKeyz.py
@@ -12,268 +12,265 @@ import shutil
 status_array = []
 
 def main():
-    
-    status_array = []
-    # Initial Start Date and Time of Program
-    now = datetime.now()
-    dt_string = now.strftime("%m_%d_%Yat%I_%M_%S%p")
-    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S%p")
-
-
-
-    # ERROR-HANDLING with pyinstaller executable, determines if application is a script file or frozen exe
-    if getattr(sys, 'frozen', False):
-        application_path = os.path.dirname(sys.executable)
-    elif __file__:
-        application_path = os.path.dirname(__file__)
-
-
-    # temp directory path
-    temp_path = os.path.join(application_path, "temp")
-
-    for item in os.listdir(temp_path):
-          item_path = os.path.join(temp_path, item)
-          if os.path.isfile(item_path):  # Check if it's a file
-               os.remove(item_path)  # Remove the file
-          elif os.path.isdir(item_path):  # Check if it's a directory
-               shutil.rmtree(item_path)  # Remove the directory and its contents
-
-
-
-    #config file name
-    config_name = 'config.txt'
-
-    # Path for config file (which stores the path for the output folder)
-    config_path = os.path.join(application_path, config_name)   
-
-
-    # Reading the config file for the path to the output folder and storing to variable
-    configFile = open(config_path,"r+")
-    outputPathInConfig = configFile.read()    
-
-    # Saving the path for output, summary, and event file
-    outputFile = outputPathInConfig + "\\"
-    outputSummaryFile = outputFile
-    outputEventFile = outputFile
-
-    # Printing output file directory, and allowing user to change destination directory
-    # If changed, the output directory is saved by modifying the config file
-    print("The output files directory is set to \n\n" + outputFile + "\n")
-    output_path = input("Press enter to ignore, or type in the complete directory: ")
-    print(output_path)
-    if (output_path != ""):
-         configFilePath = output_path
-         configFile = open(config_path,"w")
-         configFile.write(configFilePath)
-         outputFile = configFilePath + "\\"
-         outputSummaryFile = outputFile
-         outputEventFile = outputFile
-    else:
-         outputFile = outputFile
-         outputSummaryFile = outputFile
-         outputEventFile = outputFile
-         
-    # Creates the full file name for the output, summary, and event output files
-    outputFile += dt_string + ".txt"
-    outputSummaryFile += dt_string + "_Summary.txt"
-    outputEventFile += dt_string + "_Event_Log.txt"
-
-    # User inputs testing time length
-    hours = 8
-    print("How many hours would you like to test for?")
-    test_hours = float(input("Type in the correct hours: "))
-    if (test_hours != ""):
-         hours = test_hours
-    print(hours)
-
-    # Scans network for controllers (allows for 192.168.0.x and 192.168.1.x)
-    print("Scanning network for controllers")
-    ip_addr = "192.168.1.1/23"
-    scanned_output = scan(ip_addr)
-
-    # Date and time of when Ping Testing Begins
-    now = datetime.now()
-    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
-    start_time = now.strftime("%I:%M:%S %p")
-
-    print("Testing the following IP Addresses " + dt_print + "\n")  
-    ip_addresses = ip_results(scanned_output)
-    print("Number of Controllers:",len(ip_addresses))
-    controllers = float(len(ip_addresses))
-
-    # Calculating how many packets to send based on number of targets and available thread count
-    # to fit in allotted time frame
-    global seconds
-    seconds = float(3600 * hours)
-
-    global packets
-    threads = 12
-    #Always Rounding Up Segments (Divisions due to limitations on thread count)
-    segments = float(controllers/threads)
-    if segments != float(int(controllers/threads)):
-         segments = int(segments)
-         segments += 1
-    else:
-         segments = int(segments)
-
-
-    if int(segments) == 0:
-        packets = int(seconds/1)
-    else:
-        packets = int(seconds/float(segments))
-
-
-    ip_packet = []
-    for i in ip_addresses:
-         ip_packet.append([i,packets])
-
-
-
-    # execute ping test on all ip addresses simultaneously (limited by thread count)
-    with Pool() as pool:
-        pool.starmap(ping, ip_packet)
-
-    # date and time of end of ping test
-    now = datetime.now()
-    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
-    end_time = now.strftime("%I:%M:%S %p")
-    print("\nPing Test On All IP Addresses Completed " + dt_print + "\n")
-
-    # Summary File Header
-    with open(outputSummaryFile, "a") as file:
-        file.write("Summary Results\n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n\n")
-
-    # Event Log Header
-    with open(outputEventFile, "a") as file:
-         file.write("Event Log \n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n\n")
-
-    number_passed = 0
-    # Writs the normal output file
-    time_elapsed = ""
-
-    for each_file in os.listdir(temp_path):
-         each_file = os.path.join(temp_path, each_file)
-         with open(each_file, "r") as source_file, open(outputFile, "a") as destination_file:
-               output = source_file.read()
-               destination_file.write(output)
-          # IP Address
- 
-         ip_start = output.find("Pinging ")
-         ip_end = output.find(" with 32 bytes of data")
-         ip = output[ip_start + 8: ip_end]
-
-        # Pass or fail
-
-         pass_or_fail = output.find("RESULT IS FAIL")
-         if pass_or_fail == -1:
-             test = "PASS"
-             number_passed += 1
-         else:
-             test = "FAIL"
-
-         test_start = output.find("(")
-         test_end = output.find("%")
-
-         loss = output[test_start + 1: test_end + 6]
-
-        #CHECKs if it was ever disconnected, and changes message to FAIL (WAS DISCONNECTED)
-         unreachable = output.find("DISCONNECTED")
-         if unreachable != -1:
-             test += " (WAS DISCONNECTED)"
-             loss = "N/A"
-
-        # Finding Event Log
-         event_start = output.find("EVENT LOG")
-         event_end = output.find("END LOG")
-         event = output[event_start: event_end]
-
-
-         with open(outputSummaryFile, "a") as file:
-            file.write(ip + "\t\t" + loss + "\t\t" + test + "\n")
-
-         with open(outputEventFile, "a") as file:
-             file.write(ip + "\n" + event + "\n")
      
+     status_array = []
 
-    """ for output in outputs:
-        with open(outputFile, "a") as file:
-            file.write(output)
-
-        # Writes the summary file with pass or fail status, %loss, and IP
-        # start = output.find("Ping statistics")
-        # end = output.find("Average")
-        # outputSummary = output[start: (end+13)]
-        
-        # IP Address
-        ip_start = output.find("Pinging ")
-        ip_end = output.find(" with 32 bytes of data")
-        ip = output[ip_start + 8: ip_end]
-
-        # Pass or fail
-
-        pass_or_fail = output.find("RESULT IS FAIL")
-        if pass_or_fail == -1:
-             test = "PASS"
-             number_passed += 1
-        else:
-             test = "FAIL"
+     # Get the inital time and date for the start of the program
+     now = datetime.now()
+     dt_string = now.strftime("%m_%d_%Yat%I_%M_%S%p")
+     dt_print = now.strftime("%m/%d/%Y at %I:%M:%S%p")
 
 
-        # Check if unreachable/interrupt
-
-        #interrupt = output.find("unreachable")
-        #if interrupt != -1:
-             #fail_time_start = output.find("time: ")
-             #fail_time_end = output.find("PST")
-             #test = "FAIL --- DEVICE WAS INTERRUPTED/DISCONNECTED AT " + output[fail_time_start + 5: fail_time_end]
+     # ERROR-HANDLING with pyinstaller executable, determines if application is a script file or frozen exe
+     if getattr(sys, 'frozen', False):
+          application_path = os.path.dirname(sys.executable)
+     elif __file__:
+          application_path = os.path.dirname(__file__)
 
 
-        test_start = output.find("(")
-        test_end = output.find("%")
+     # temp directory path (this directory will hold ping test results for each device tested in separate files)
+     temp_path = os.path.join(application_path, "temp")
 
-        # if output[test_start + 1: test_end] == "0":
-        #      test = "PASS"
-        #      number_passed += 1
-        # else:
-        #      test = "FAIL"
-        #      # for x in range(output.count("LINE"))
-        #      time_start = output.find("O")
-        #      time_end = output.find(" s")
-        #      time_elapsed += "\n" + ip + "\t" + output[time_start: time_end + 2]
+     # Deleting all contents of temp directory  
+     for item in os.listdir(temp_path):
+               item_path = os.path.join(temp_path, item)
+               if os.path.isfile(item_path):  # Check if it's a file
+                    os.remove(item_path)  # Remove the file
+               elif os.path.isdir(item_path):  # Check if it's a directory
+                    shutil.rmtree(item_path)  # Remove the directory and its contents
+
+     #config file name
+     config_name = 'config.txt'
+
+     # Path for config file (which stores the path for the output folder and more...)
+     config_path = os.path.join(application_path, config_name)   
+
+     # Reading the config file for the path to the output folder and storing to variable
+     config_file = open(config_path,"r+")
+     output_path_from_config = config_file.read()    
+
+     # Saving the path for output, summary, and event file
+     output_data_file = output_path_from_config + "\\"
+     output_summary_file = output_data_file
+     output_event_file = output_data_file
+
+     # Printing output file directory, and allowing user to change destination directory
+     # If changed, the output directory is saved by modifying the config file
+     print("The output files directory is set to \n\n" + output_data_file + "\n")
+     output_path = input("Press enter to ignore, or type in the complete directory: ")
+     print(output_path)
+     if (output_path != ""):
+          output_path_from_config = output_path
+          with open(config_path, "w") as file:
+               file.write(output_path_from_config)
+          output_data_file = output_path_from_config + "\\"
+          output_summary_file = output_data_file
+          output_event_file = output_data_file
+     else:
+          output_data_file = output_data_file
+          output_summary_file = output_data_file
+          output_event_file = output_data_file
+          
+     # Creates the full file name for the output, summary, and event output files
+     output_data_file += dt_string + ".txt"
+     output_summary_file += dt_string + "_Summary.txt"
+     output_event_file += dt_string + "_Event_Log.txt"
+
+     # User inputs testing time length
+     hours = 8
+     print("How many hours would you like to test for?")
+     test_hours = float(input("Type in the correct hours: "))
+     if (test_hours != ""):
+          hours = test_hours
+     print(hours)
+
+     # Scans network for controllers (allows for 192.168.0.x and 192.168.1.x)
+     print("Scanning network for devices")
+     ip_addr = "192.168.1.1/23"
+     scanned_output = scan(ip_addr)
+
+     # Date and time of when Ping Testing Begins
+     now = datetime.now()
+     dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
+     start_time = now.strftime("%I:%M:%S %p")
+
+     # Prints the list of IP addresses and MAC addresses of devices being tested
+     print("Testing the following IP Addresses " + dt_print + "\n")  
+     ip_addresses = ip_results(scanned_output)
+     print("Number of Devices:",len(ip_addresses))
+     devices = float(len(ip_addresses))
+
+     # Calculating how many packets to send based on number of targets and available thread count
+     # to fit in allotted time frame
+     seconds = float(3600 * hours)
+     threads = 12
+
+     #Always Rounding Up Segments (Divisions due to limitations on thread count)
+     segments = float(devices/threads)
+     if segments != float(int(devices/threads)):
+          segments = int(segments)
+          segments += 1
+     else:
+          segments = int(segments)
 
 
-        loss = output[test_start + 1: test_end + 6]
-
-        #CHECKs if it was ever disconnected, and changes message to FAIL (WAS DISCONNECTED)
-        unreachable = output.find("DISCONNECTED")
-        if unreachable != -1:
-             test += " (WAS DISCONNECTED)"
-             loss = "N/A"
-
-        # Finding Event Log
-        event_start = output.find("EVENT LOG")
-        event_end = output.find("END LOG")
-        event = output[event_start: event_end]
+     if int(segments) == 0:
+          packets = int(seconds/1)
+     else:
+          packets = int(seconds/float(segments))
 
 
-        with open(outputSummaryFile, "a") as file:
-            file.write(ip + "\t\t" + loss + "\t\t" + test + "\n")
+     ip_packet = []
+     for i in ip_addresses:
+          ip_packet.append([i,packets])
 
-        with open(outputEventFile, "a") as file:
-             file.write(ip + "\n" + event + "\n") """
 
-    with open(outputSummaryFile, "a") as file:
-        file.write("\n" + str(number_passed) + " out of " + str(int(controllers)) + " controllers PASSED\n\nEND TIME: " + end_time + "\n\n" + str(status_array))
 
-    with open(outputEventFile, "a") as file:
-         file.write("END TIME: " + end_time)
+     # execute ping test on all ip addresses simultaneously (limited by thread count)
+     with Pool() as pool:
+          pool.starmap(ping, ip_packet)
 
-    # End date and time
-    now = datetime.now()
-    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
-    
-    print("Finished " + dt_print + "\n")
-    input()
+     # date and time of end of ping test
+     now = datetime.now()
+     dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
+     end_time = now.strftime("%I:%M:%S %p")
+     print("\nPing Test On All IP Addresses Completed " + dt_print + "\n")
+
+     # Summary File Header
+     with open(output_summary_file, "a") as file:
+          file.write("Summary Results\n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n\n")
+
+     # Event Log Header
+     with open(output_event_file, "a") as file:
+          file.write("Event Log \n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n\n")
+
+     number_passed = 0
+     # Writs the normal output file
+     time_elapsed = ""
+
+     for each_file in os.listdir(temp_path):
+          each_file = os.path.join(temp_path, each_file)
+          with open(each_file, "r") as source_file, open(output_data_file, "a") as destination_file:
+                    output = source_file.read()
+                    destination_file.write(output)
+               # IP Address
+     
+          ip_start = output.find("Pinging ")
+          ip_end = output.find(" with 32 bytes of data")
+          ip = output[ip_start + 8: ip_end]
+
+          # Pass or fail
+
+          pass_or_fail = output.find("RESULT IS FAIL")
+          if pass_or_fail == -1:
+               test = "PASS"
+               number_passed += 1
+          else:
+               test = "FAIL"
+
+          test_start = output.find("(")
+          test_end = output.find("%")
+
+          loss = output[test_start + 1: test_end + 6]
+
+          #CHECKs if it was ever disconnected, and changes message to FAIL (WAS DISCONNECTED)
+          unreachable = output.find("DISCONNECTED")
+          if unreachable != -1:
+               test += " (WAS DISCONNECTED)"
+               loss = "N/A"
+
+          # Finding Event Log
+          event_start = output.find("EVENT LOG")
+          event_end = output.find("END LOG")
+          event = output[event_start: event_end]
+
+
+          with open(output_summary_file, "a") as file:
+               file.write(ip + "\t\t" + loss + "\t\t" + test + "\n")
+
+          with open(output_event_file, "a") as file:
+               file.write(ip + "\n" + event + "\n")
+          
+
+     """ for output in outputs:
+          with open(outputFile, "a") as file:
+               file.write(output)
+
+          # Writes the summary file with pass or fail status, %loss, and IP
+          # start = output.find("Ping statistics")
+          # end = output.find("Average")
+          # outputSummary = output[start: (end+13)]
+          
+          # IP Address
+          ip_start = output.find("Pinging ")
+          ip_end = output.find(" with 32 bytes of data")
+          ip = output[ip_start + 8: ip_end]
+
+          # Pass or fail
+
+          pass_or_fail = output.find("RESULT IS FAIL")
+          if pass_or_fail == -1:
+               test = "PASS"
+               number_passed += 1
+          else:
+               test = "FAIL"
+
+
+          # Check if unreachable/interrupt
+
+          #interrupt = output.find("unreachable")
+          #if interrupt != -1:
+               #fail_time_start = output.find("time: ")
+               #fail_time_end = output.find("PST")
+               #test = "FAIL --- DEVICE WAS INTERRUPTED/DISCONNECTED AT " + output[fail_time_start + 5: fail_time_end]
+
+
+          test_start = output.find("(")
+          test_end = output.find("%")
+
+          # if output[test_start + 1: test_end] == "0":
+          #      test = "PASS"
+          #      number_passed += 1
+          # else:
+          #      test = "FAIL"
+          #      # for x in range(output.count("LINE"))
+          #      time_start = output.find("O")
+          #      time_end = output.find(" s")
+          #      time_elapsed += "\n" + ip + "\t" + output[time_start: time_end + 2]
+
+
+          loss = output[test_start + 1: test_end + 6]
+
+          #CHECKs if it was ever disconnected, and changes message to FAIL (WAS DISCONNECTED)
+          unreachable = output.find("DISCONNECTED")
+          if unreachable != -1:
+               test += " (WAS DISCONNECTED)"
+               loss = "N/A"
+
+          # Finding Event Log
+          event_start = output.find("EVENT LOG")
+          event_end = output.find("END LOG")
+          event = output[event_start: event_end]
+
+
+          with open(outputSummaryFile, "a") as file:
+               file.write(ip + "\t\t" + loss + "\t\t" + test + "\n")
+
+          with open(outputEventFile, "a") as file:
+               file.write(ip + "\n" + event + "\n") """
+
+     with open(output_summary_file, "a") as file:
+          file.write("\n" + str(number_passed) + " out of " + str(int(devices)) + " controllers PASSED\n\nEND TIME: " + end_time + "\n\n" + str(status_array))
+
+     with open(output_event_file, "a") as file:
+          file.write("END TIME: " + end_time)
+
+     # End date and time
+     now = datetime.now()
+     dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
+     
+     print("Finished " + dt_print + "\n")
+     input()
 
 # Performs Ping Test on each IP Address
 def ping (host, packets):
